b85bba3ead9e5a1ca98ca5cc995531bb72336b26
Breno Alves <breno.alvs@hotmail.com>
2015-07-06 16:43:38 -0300

    Added some initial theme files

---
+++ wp-content/themes/wp-rio/front-page.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>WP Rio</title>
+</head>
+<body>
+	<p>Tema inicial do WP Rio</p>
+</body>
+</html>
---
+++ wp-content/themes/wp-rio/index.php
@@ -0,0 +1,7 @@
+<?php
+
+if ( ! defined( 'WPINC' ) ) : 
+	exit;
+
+// Redirect for front-page.php
+wp_redirect( home_url() ); exit;
