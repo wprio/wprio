247b9b15668bd651edcdcf4db3f3e18a2f828e87
Breno Alvs <breno.alvs@hotmail.com>
2015-08-23 02:34:46 -0300

    Container alterado

--- wp-content/themes/wp-rio/front-page.php
+++ wp-content/themes/wp-rio/front-page.php
@@ -13,83 +13,85 @@
 		</div><!-- .banner-caption -->
 	</section><!-- .header-caption -->
 
-	<section class="next-event">
-		<!-- events loop -->
-		<article class="event">
-			<figure class="event-image"></figure>
-			<div class="event-content">
-				<h3 class="event-title">WordCamp Rio de Janeiro 2015</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum placeat voluptate accusantium dignissimos ducimus architecto numquam natus consectetur odit quod!</p>
-				<a href="#" class="event-link">Saiba mais desse evento</a>
-			</div>
-		</article><!-- .event -->
-	</section><!-- .next-event -->
-
-	<section class="next-meetup">
-		<article class="meetup">
-			<figure class="meetup-image"></figure>
-			<div class="meetup-content">
-				<h3 class="meetup-title">10º WordPress Meetup</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus facere velit magni dicta, corporis quod doloremque. Rerum cumque distinctio magnam!</p>
-				<a href="#" class="meetup-link">Saiba mais desse evento</a>
-			</div>
-		</article>
-
-		<div class="slides">
-			<h3>Procurando os slides desse encontro?</h3>
-
-			<div class="slides-container">
-				<div class="slide">
-					<figure class="slide-image"></figure>
-					<h4 class="slide-title">Lorem ipsum dolor sit.</h4>
+	<div class="container">
+		<section class="next-event">
+			<!-- events loop -->
+			<article class="event">
+				<figure class="event-image"></figure>
+				<div class="event-content">
+					<h3 class="event-title">WordCamp Rio de Janeiro 2015</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum placeat voluptate accusantium dignissimos ducimus architecto numquam natus consectetur odit quod!</p>
+					<a href="#" class="event-link">Saiba mais desse evento</a>
 				</div>
-				<div class="slide">
-					<figure class="slide-image"></figure>
-					<h4 class="slide-title">Lorem ipsum dolor sit.</h4>
+			</article><!-- .event -->
+		</section><!-- .next-event -->
+
+		<section class="next-meetup">
+			<article class="meetup">
+				<figure class="meetup-image"></figure>
+				<div class="meetup-content">
+					<h3 class="meetup-title">10º WordPress Meetup</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus facere velit magni dicta, corporis quod doloremque. Rerum cumque distinctio magnam!</p>
+					<a href="#" class="meetup-link">Saiba mais desse evento</a>
 				</div>
-				<div class="slide">
-					<figure class="slide-image"></figure>
-					<h4 class="slide-title">Lorem ipsum dolor sit.</h4>
+			</article>
+
+			<div class="slides">
+				<h3>Procurando os slides desse encontro?</h3>
+
+				<div class="slides-container">
+					<div class="slide">
+						<figure class="slide-image"></figure>
+						<h4 class="slide-title">Lorem ipsum dolor sit.</h4>
+					</div>
+					<div class="slide">
+						<figure class="slide-image"></figure>
+						<h4 class="slide-title">Lorem ipsum dolor sit.</h4>
+					</div>
+					<div class="slide">
+						<figure class="slide-image"></figure>
+						<h4 class="slide-title">Lorem ipsum dolor sit.</h4>
+					</div>
 				</div>
-			</div>
-		</div><!--.slides -->
-	</section><!-- .next-meetup -->
+			</div><!--.slides -->
+		</section><!-- .next-meetup -->
 
-	<section class="partners">
-		<div class="partner"></div>
-		<div class="partner"></div>
-		<div class="partner"></div>
-		<div class="partner"></div>
+		<section class="partners">
+			<div class="partner"></div>
+			<div class="partner"></div>
+			<div class="partner"></div>
+			<div class="partner"></div>
 
-		<p>Quer colaborar com a Comunidade WordPress Carioca?</p>
-	</section><!-- .partners -->
+			<p>Quer colaborar com a Comunidade WordPress Carioca?</p>
+		</section><!-- .partners -->
 
-	<section class="contact-area">
-		<div class="blog">
-			<h2>Direto do blog</h2>
-			<article class="blog-entry">
-				<figure class="blog-entry-image"></figure>
-				<h4 class="blog-entry-title">O juíz apita e começa a partida</h4>
-				<p class="blog-entry-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, ratione.</p>
-				<a class="blog-entry-link" href="#">Leia esse artigo completo</a>
-			</article><!-- .blog-entry -->
-		</div><!-- .blog -->
+		<section class="contact-area">
+			<div class="blog">
+				<h2>Direto do blog</h2>
+				<article class="blog-entry">
+					<figure class="blog-entry-image"></figure>
+					<h4 class="blog-entry-title">O juíz apita e começa a partida</h4>
+					<p class="blog-entry-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, ratione.</p>
+					<a class="blog-entry-link" href="#">Leia esse artigo completo</a>
+				</article><!-- .blog-entry -->
+			</div><!-- .blog -->
 
-		<div class="contact">
-			<h2>Nossas Redes</h2>
-			<ul class="social-medias">
-				<li><a href="#">Facebook</a></li>
-				<li><a href="#">Twitter</a></li>
-				<li><a href="#">Google +</a></li>
-				<li><a href="#">Instagram</a></li>
-				<li><a href="#">Meetup</a></li>
-				<li><a href="#">GitHub</a></li>
-				<li><a href="#">YouTube</a></li>
-			</ul><!-- .social-medias -->
+			<div class="contact">
+				<h2>Nossas Redes</h2>
+				<ul class="social-medias">
+					<li><a href="#">Facebook</a></li>
+					<li><a href="#">Twitter</a></li>
+					<li><a href="#">Google +</a></li>
+					<li><a href="#">Instagram</a></li>
+					<li><a href="#">Meetup</a></li>
+					<li><a href="#">GitHub</a></li>
+					<li><a href="#">YouTube</a></li>
+				</ul><!-- .social-medias -->
 
-			<h3>Fale com a comunidade</h3>
-			<!-- formulario -->
-		</div><!-- .contact -->
-	</section><!-- .contact-area -->
+				<h3>Fale com a comunidade</h3>
+				<!-- formulario -->
+			</div><!-- .contact -->
+		</section><!-- .contact-area -->
+	</div><!-- .container -->
 
 <?php get_footer(); ?>
