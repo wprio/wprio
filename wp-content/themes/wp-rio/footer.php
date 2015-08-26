1ac455572600aef0fea68ca1633df37e753218aa
Breno Alves <breno.alvs@hotmail.com>
2015-08-26 19:30:03 -0300

    Próximos eventos e eventos anteriores

--- wp-content/themes/wp-rio/footer.php
+++ wp-content/themes/wp-rio/footer.php
@@ -12,7 +12,19 @@
 <footer class="main-footer">
 	<div class="container">
 	    <span class="copyright">Comunidade WordPress Rio de Janeiro &copy;</span>
-	    <span class="credits">Código é poesia</span>
+	    <span class="credits">
+	    	Código é poesia
+	    	
+	    	<ul class="social-medias">
+				<li><a href="#">Facebook</a></li>
+				<li><a href="#">Twitter</a></li>
+				<li><a href="#">Google +</a></li>
+				<li><a href="#">Instagram</a></li>
+				<li><a href="#">Meetup</a></li>
+				<li><a href="#">GitHub</a></li>
+				<li><a href="#">YouTube</a></li>
+			</ul><!-- .social-medias -->
+	    </span>
     </div><!-- .container -->
 </footer><!-- .main-footer -->
 
--- wp-content/themes/wp-rio/front-page.php
+++ wp-content/themes/wp-rio/front-page.php
@@ -21,13 +21,13 @@
 
 	<div class="container">
 		<?php
-			$events_args = array( 'post_type' => 'wordcamps', 'edit_posts_per_page' => 1 );
-			$events = new WP_Query( $events_args );
-			if ( $events->have_posts() ) :
+			$events = get_field( 'objeto-proximo-evento', 'options' ); 
+			if ( $events ) :
 		?>
 		<section class="next-event">
-			<!-- events loop -->
-			<?php while ( $events->have_posts() ) : $events->the_post(); ?>
+			<h2><?php the_field( 'titulo-proximo-evento', 'options' ); ?></h2>
+
+			<?php foreach ( $events as $post ) : setup_postdata( $post ); ?>
 			<article class="event">
 				<figure class="event-image">
 					<?php the_post_thumbnail( 'entry-thumb' ); ?>
@@ -38,17 +38,16 @@
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="btn btn-default">Saiba mais desse evento</a>
 				</div>
 			</article><!-- .event -->
-			<?php endwhile; ?>
+			<?php endforeach; wp_reset_postdata(); ?>
 		</section><!-- .next-event -->
 		<?php endif; ?>
 		
 		<?php
-			$meetups_args = array( 'post_type' => 'meetups', 'edit_posts_per_page' => 1 );
-			$meetups = new WP_Query( $meetups_args );
-			if ( $meetups->have_posts() ) :
+			$meetups = get_field( 'objeto-evento-passado', 'options' ); 
+			if ( $meetups ) :
 		?>
 		<section class="last-meetup">
-			<?php while ( $meetups->have_posts() ) : $meetups->the_post(); ?>
+			<?php foreach ( $meetups as $post ) : setup_postdata( $post ); ?>
 			<article class="meetup">
 				<figure class="meetup-image">
 					<?php the_post_thumbnail( 'entry-thumb' ); ?>
@@ -60,24 +59,28 @@
 				</div>
 			</article>
 
-			<?php if ( have_rows( 'palestras-meetup' ) ) : ?>
+			<?php if ( get_field( 'exibir-slides', 'options' ) ) : ?>
 			<div class="slides">
-				<h3>Procurando os slides desse encontro?</h3>
+				<?php
+					$slides = get_field( 'objeto-evento-passado', 'options' ); 
+					if ( $slides ) :
+				?>	
+				<h3><?php the_field( 'titulo-slides', 'options' ); ?></h3>
 
 				<div class="slides-container">
-					<?php while ( have_rows( 'palestras-meetup' ) ) : ?>
+					<?php foreach ( $slides as $post ) : setup_postdata( $post ); ?>
 					<div class="slide">
 						<figure class="slide-image">
 							<?php the_sub_field( 'imagem-capa-meetup' ); ?>
 						</figure>
 						<h4 class="slide-title"><?php the_sub_field( 'titulo-meetup' ); ?></h4>
 					</div>
-					<?php endwhile; ?>
+					<?php endforeach; wp_reset_postdata(); ?>
 				</div>
 			</div><!--.slides -->
 			<?php endif; ?>
 
-			<?php endwhile; ?>
+			<?php endforeach; wp_reset_postdata(); ?>
 		</section><!-- .last-meetup -->
 		<?php endif; ?>
 		
@@ -91,8 +94,8 @@
 						$partner_logo = wp_get_attachment_image_src( get_sub_field( 'logo-parceiro', 'options' ), 'partner-thumb' );
 					?>
 				<li class="partner">
-					<a href="<?php the_sub_field( 'link-parceiro', 'options' ); ?>" title="<?php the_sub_field( 'nome-parceiro', 'options' ); ?>">
-					<img src="<?php echo $partner_logo['url']; ?>" alt="<?php the_sub_field( 'nome-parceiro', 'options' ); ?>">
+					<a href="<?php the_sub_field( 'link-parceiro', 'options' ); ?>">
+					<img src="<?php echo $partner_logo['url']; ?>">
 					</a>
 				</li>
 				<?php endwhile; ?>
@@ -103,42 +106,22 @@
 		</section><!-- .partners -->
 		<?php endif; ?>
 
-		<section class="contact-area">
-			<div class="contact-wrapper">
-				<div class="blog">
-					<h2>Direto do blog</h2>
+		<?php if ( have_posts() ) : ?>
+		<section class="blog-area">
+			<h2>Direto do blog</h2>
 
-					<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
-					<article class="blog-entry">
-						<figure class="blog-entry-image">
-							<?php the_post_thumbnail( 'entry-thumb' ); ?>
-						</figure>
-						<h3 class="blog-entry-title"><?php the_title(); ?></h3>
-						<p class="blog-entry-excerpt"><?php the_excerpt(); ?></p>
-						<a class="btn btn-default" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">Leia esse artigo completo</a>
-					</article><!-- .blog-entry -->
-					<?php endwhile; else : ?>
-					<p>Desculpe, mas nenhum post foi encontrado. :(</p>
-					<?php endif; ?>
-				</div><!-- .blog -->
-
-				<div class="contact">
-					<h2>Nossas Redes</h2>
-					<ul class="social-medias">
-						<li><a href="#">Facebook</a></li>
-						<li><a href="#">Twitter</a></li>
-						<li><a href="#">Google +</a></li>
-						<li><a href="#">Instagram</a></li>
-						<li><a href="#">Meetup</a></li>
-						<li><a href="#">GitHub</a></li>
-						<li><a href="#">YouTube</a></li>
-					</ul><!-- .social-medias -->
-
-					<h2>Fale com a comunidade</h2>
-					<!-- formulario -->
-				</div><!-- .contact -->
-			</div><!-- .contact-wrapper -->
-		</section><!-- .contact-area -->
+			<?php while( have_posts() ) : the_post(); ?>
+			<article class="blog-entry">
+				<figure class="blog-entry-image">
+					<?php the_post_thumbnail( 'entry-thumb' ); ?>
+				</figure>
+				<h3 class="blog-entry-title"><?php the_title(); ?></h3>
+				<p class="blog-entry-excerpt"><?php the_excerpt(); ?></p>
+				<a class="btn btn-default" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">Leia esse artigo completo</a>
+			</article><!-- .blog-entry -->
+			<?php endwhile; ?>
+		</section><!-- .blog-area -->
+		<?php endif; ?>
 	</div><!-- .container -->
 
 <?php get_footer(); ?>
