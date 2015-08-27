4d3f9d0be6aca2f32aa5c099890d6a638b45d34a
Felipe Pavão <contact@felipepavao.com>
2015-08-27 15:59:07 -0300

    trocando o conteudo de pagina para outra pagina

--- wp-content/themes/wp-rio/template-quem-somos.php
+++ wp-content/themes/wp-rio/template-quem-somos.php
@@ -2,23 +2,53 @@
 /* Template Name: Quem Somos */
 get_header(); ?>
 
-	<main id="content" class="<?php echo odin_classes_page_full(); ?>" tabindex="-1" role="main">
-
+	<div class="container internal">	
+		<main id="main-content" class="site-main" role="main">
 			<?php
-				// Start the Loop.
 				while ( have_posts() ) : the_post();
-
-					// Include the page content template.
 					get_template_part( 'content', 'page' );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
 				endwhile;
 			?>
+			<?php 
+				global $wpdb;
+				$query = "SELECT ID, user_nicename from $wpdb->users ORDER BY user_nicename";
+				$author_ids = $wpdb->get_results($query);
+				if ( $author_ids ):
+			?>
+					<hr />
+					<section class="players">
+						<h2>Escala&ccedil;&atilde;o do time WP-Rio</h2>
+						<?php
+							
+							foreach($author_ids as $author) :
+								
+								$curauth = get_userdata($author->ID);
+								
+								if($curauth->user_level > 0 || $curauth->user_login == 'admin') :
+									$user_link = get_author_posts_url($curauth->ID);
+									$avatar = 'wavatar';
 
-	</main><!-- #main -->
+						?>		<div class="row">
+
+									<div class="col-md-4">
+										<?php echo get_avatar($curauth->user_email, '224', $avatar); ?>
+									</div>
+									<div class="col-md-8">
+										<h3 class="post-title"><?php echo $curauth->display_name; ?></h3>
+
+										<p><?php echo $curauth->description; ?></p>
+									</div>
+								</div>
+						<?php
+								endif;
+							endforeach;
+						?>
+					</section>
+			<?php
+				endif;
+			?>
+		</main><!-- #main -->
+	</div>
 
 <?php
 get_footer();
