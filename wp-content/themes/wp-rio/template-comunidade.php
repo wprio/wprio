5aad26d10871aaca6e747020c74133a3f98e594c
Felipe Pavão <contact@felipepavao.com>
2015-08-26 21:18:27 -0300

    adding backend of team players page

---
+++ wp-content/themes/wp-rio/template-comunidade.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Template Name: Template Comunidade
+ * The Template for displaying all meetups
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+
+get_header(); ?>
+
+	<section class="full-banner">
+		<?php if ( has_post_thumbnail() ) : ?>
+		
+		<?php endif; ?>
+	</section><!-- .header-caption -->
+
+	<div class="container internal">	
+		<main id="main-content" class="site-main" role="main">
+			<?php
+				while ( have_posts() ) : the_post();
+					get_template_part( 'content', 'page' );
+				endwhile;
+			?>
+			<?php 
+				global $wpdb;
+				$query = "SELECT ID, user_nicename from $wpdb->users ORDER BY user_nicename";
+				$author_ids = $wpdb->get_results($query);
+				if ( $author_ids ):
+			?>
+					<hr />
+					<section class="players">
+						<h2>Escala&ccedil;&atilde;o do time WP-Rio</h2>
+						<?php
+							
+							foreach($author_ids as $author) :
+								
+								$curauth = get_userdata($author->ID);
+								
+								if($curauth->user_level > 0 || $curauth->user_login == 'admin') :
+									$user_link = get_author_posts_url($curauth->ID);
+									$avatar = 'wavatar';
+
+						?>		<div class="row">
+
+									<div class="col-md-4">
+										<?php echo get_avatar($curauth->user_email, '224', $avatar); ?>
+									</div>
+									<div class="col-md-8">
+										<h3 class="post-title"><?php echo $curauth->display_name; ?></h3>
+
+										<p><?php echo $curauth->description; ?></p>
+									</div>
+								</div>
+						<?php
+								endif;
+							endforeach;
+						?>
+					</section>
+			<?php
+				endif;
+			?>
+		</main><!-- #main -->
+	</div>
+
+<?php
+get_footer();
