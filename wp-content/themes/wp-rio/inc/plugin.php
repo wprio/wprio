bc1152727839f1305143c491a18658b039513936
Guga Alves <tudoparawordpress@gmail.com>
2015-08-28 12:17:12 -0300

    Campos do ACF exportados pro plugin.php

--- wp-content/themes/wp-rio/inc/plugin.php
+++ wp-content/themes/wp-rio/inc/plugin.php
@@ -2363,4 +2363,103 @@ acf_add_local_field_group(array (
 	));
 }
 
+if( function_exists('acf_add_local_field_group') ){
+
+acf_add_local_field_group(array (
+	'key' => 'group_55e07604ce277',
+	'title' => 'Texto do Blog',
+	'fields' => array (
+		array (
+			'key' => 'field_55e076351c52d',
+			'label' => 'Exibir título e descrição no blog?',
+			'name' => 'boolean-blog',
+			'type' => 'true_false',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'message' => '',
+			'default_value' => 0,
+		),
+		array (
+			'key' => 'field_55e0760c1c52c',
+			'label' => 'Título do Blog',
+			'name' => 'titulo-blog',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array (
+				array (
+					array (
+						'field' => 'field_55e076351c52d',
+						'operator' => '==',
+						'value' => '1',
+					),
+				),
+			),
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+			'readonly' => 0,
+			'disabled' => 0,
+		),
+		array (
+			'key' => 'field_55e0765d1c52e',
+			'label' => 'Descrição do Blog',
+			'name' => 'descriçao-blog',
+			'type' => 'wysiwyg',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array (
+				array (
+					array (
+						'field' => 'field_55e076351c52d',
+						'operator' => '==',
+						'value' => '1',
+					),
+				),
+			),
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'options_page',
+				'operator' => '==',
+				'value' => 'acf-options',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+}
+
 define( 'ACF_LITE' , false );
