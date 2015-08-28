30aa0bf1716fe83ddd7f1df481e44f87cea06d10
Guga Alves <tudoparawordpress@gmail.com>
2015-08-28 17:41:16 -0300

    Tags de Analytics e Google Search Console

--- wp-content/themes/wp-rio/header.php
+++ wp-content/themes/wp-rio/header.php
@@ -14,8 +14,19 @@
     <meta charset="UTF-8">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
+    <meta name="google-site-verification" content="9Ms0xe7SDf46KRJFPVCpyZpXu_MBKedoBR8Quf1bPzc" />
 </head>
 <body <?php body_class(); ?>>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-66888786-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
     <nav class="main-nav">
         <a id="logo" href="<?php echo home_url( '/' ); ?>" title="Ir para Home">
             <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?>" alt="Logo WP Rio">
