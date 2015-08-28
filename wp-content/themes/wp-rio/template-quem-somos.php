ea9e0b4e500daf3a1ba2868b0a376af058eb3f6e
Felipe Pavão <contact@felipepavao.com>
2015-08-28 15:50:22 -0300

    fixing template quem somos

--- wp-content/themes/wp-rio/template-quem-somos.php
+++ wp-content/themes/wp-rio/template-quem-somos.php
@@ -8,39 +8,33 @@ get_header(); ?>
 				while ( have_posts() ) : the_post();
 					get_template_part( 'content', 'page' );
 				endwhile;
+				//wp_reset_postdata();
 			?>
 			<?php 
-				global $wpdb;
-				$query = "SELECT ID, user_nicename from $wpdb->users ORDER BY user_nicename";
-				$author_ids = $wpdb->get_results($query);
-				if ( $author_ids ):
+				$team = get_field('equipe', $post->ID);
+				if ( $team ):
 			?>
 					<hr />
 					<section class="players">
 						<h2>Escala&ccedil;&atilde;o do time WP-Rio</h2>
 						<?php
-							
-							foreach($author_ids as $author) :
-								
-								$curauth = get_userdata($author->ID);
-								
-								if($curauth->user_level > 0 || $curauth->user_login == 'admin') :
-									$user_link = get_author_posts_url($curauth->ID);
-									$avatar = 'wavatar';
-
+							foreach ( $team as $post ) : setup_postdata( $post );
 						?>		<div class="row">
-
 									<div class="col-md-4">
-										<?php echo get_avatar($curauth->user_email, '224', $avatar); ?>
+										<?php echo get_field($post->ID, 'foto-perfil'); ?>
 									</div>
 									<div class="col-md-8">
-										<h3 class="post-title"><?php echo $curauth->display_name; ?></h3>
+										<h3 class="post-title">
+											<?php echo get_field($post->ID, 'nome-pessoa'); ?>
+										</h3>
 
-										<p><?php echo $curauth->description; ?></p>
+										<p>
+											<?php echo get_field($post->ID, 'descricao'); ?>
+										</p>
 									</div>
 								</div>
 						<?php
-								endif;
+								
 							endforeach;
 						?>
 					</section>
