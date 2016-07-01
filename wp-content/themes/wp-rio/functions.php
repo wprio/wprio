3eca7370045a293c2dfde8d7129803341f7d8a4c
Anderson <theandersonn@gmail.com>
2016-07-01 01:24:05 -0300

    Ajusta analytics - fix issue #66

--- wp-content/themes/wp-rio/functions.php
+++ wp-content/themes/wp-rio/functions.php
@@ -36,6 +36,27 @@ require_once get_template_directory() . '/core/classes/class-thumbnail-resizer.p
 // require_once get_template_directory() . '/core/classes/class-post-form.php';
 // require_once get_template_directory() . '/core/classes/class-user-meta.php';
 
+
+/*--------------------------------------------------------------
+	Insere Google Analytics no Footer
+--------------------------------------------------------------*/
+add_action('wp_footer', 'insert_googleanalytics');
+
+function insert_googleanalytics() { ?>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-66888786-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+
+<?php }
+
+
+
 /**
  * Odin Widgets.
  */
