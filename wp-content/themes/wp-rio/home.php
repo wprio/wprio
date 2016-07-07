1d70d722e3d3c4878edd82b29dc24ab72d9eaf04
Anderson <theandersonn@gmail.com>
2016-07-07 00:15:01 -0300

    Ajusta o n. de posts na Home Blog

--- wp-content/themes/wp-rio/home.php
+++ wp-content/themes/wp-rio/home.php
@@ -1,17 +1,23 @@
 <?php get_header(); ?>
 	<div class="container">
-		<?php if( get_field('boolean-blog','options')) {
-		echo '<header class="blog-header">';
-			$titulo = get_field('titulo-blog','options');
-			if (!empty($titulo)){
-				echo '<h1>' . $titulo . '</h1>';
-			}
-			$texto = get_field('descricao-blog','options');
-			if (!empty($texto)){
-				echo $texto;
-			}
-		echo '</header>';
-		} ?>
+		<?php if( get_field('boolean-blog','options') ): ?>	
+			<header class="blog-header">
+			<?php
+				$titulo = get_field('titulo-blog','options');
+				
+				if ( !empty($titulo) ){
+					echo '<h1>' . $titulo . '</h1>';
+				}
+
+				$texto = get_field('descricao-blog','options');
+			
+				if (!empty($texto)){
+					echo $texto;
+				}				
+			?>	
+			</header>
+		<?php endif; ?>
+
 		<section class="blog-entries">
 			<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 				<article class="entry">
@@ -28,7 +34,11 @@
 					<?php the_excerpt(); ?>
 					<a class="entry-comments" href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
 				</article>
-			<?php endwhile; else : ?>
+			<?php endwhile; ?>
+
+			<?php echo odin_pagination(); ?>
+
+			<?php else : ?>
 				<p>Desculpe, mas ainda não há posts neste blog! :(</p>
 			<?php endif; ?>
 		</section><!-- .blog-entries -->
